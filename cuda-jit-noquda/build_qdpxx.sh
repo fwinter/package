7b9b3d8b10f5ebd07038f3d3fd712c645ae17be1
Frank Winter <fwinter@jlab.org>
2021-11-02 13:24:30 -0400

    Initial stuff

---
+++ cuda-jit-noquda/build_qdpxx.sh
@@ -0,0 +1,27 @@
+source ./env.sh
+pushd ${BUILDROOT}
+
+if [ -d ./build_qdpxx ];
+then
+  rm -rf ./build_qdpxx
+fi
+
+mkdir  ./build_qdpxx
+cd ./build_qdpxx
+
+#       -DQDP_ENABLE_COMM_SPLIT_DEVICEINIT=ON
+
+cmake ${SRCROOT}/qdp-jit \
+       -DQDP_PRECISION=double \
+       -DCMAKE_INSTALL_PREFIX=${INSTALLROOT}/qdpxx \
+       -DQMP_DIR=${INSTALLROOT}/qmp/lib/cmake/QMP \
+       -DLLVM_DIR=${INSTALLROOT}/llvm-13/lib/cmake/llvm \
+       -DQDP_ENABLE_BACKEND=CUDA \
+       -DQDP_BUILD_EXAMPLES=OFF \
+       -DQDP_ENABLE_LLVM13=ON
+
+
+make -j $(nproc)
+make install
+
+popd
---
+++ cuda-jit-noquda/env.sh
@@ -0,0 +1,8 @@
+export CXX=mpicxx
+export CC=mpicc
+
+export TOPDIR_HIP=`pwd`
+export SRCROOT=${TOPDIR_HIP}/../src
+export BUILDROOT=${TOPDIR_HIP}/build
+export INSTALLROOT=${TOPDIR_HIP}/install
+export LD_LIBRARY_PATH=${INSTALLROOT}/llvm-13/lib:${LD_LIBRARY_PATH}
