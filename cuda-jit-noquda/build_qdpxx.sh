7a0a72629eb8a06e2e323a52033b3735c4911a48
Frank Winter <fwinter@jlab.org>
2021-11-03 07:59:42 -0700

    Perlmutter ready

--- cuda-jit-noquda/build_qdpxx.sh
+++ cuda-jit-noquda/build_qdpxx.sh
@@ -11,6 +11,12 @@ cd ./build_qdpxx
 
 #       -DQDP_ENABLE_COMM_SPLIT_DEVICEINIT=ON
 
+if [ $PKG_PROP_OPT -ne "OFF" ]
+then
+    PKG_PROP_OPT="ON"
+fi
+
+
 cmake ${SRCROOT}/qdp-jit \
        -DQDP_PRECISION=double \
        -DCMAKE_INSTALL_PREFIX=${INSTALLROOT}/qdpxx \
@@ -18,7 +24,8 @@ cmake ${SRCROOT}/qdp-jit \
        -DLLVM_DIR=${INSTALLROOT}/llvm-13/lib/cmake/llvm \
        -DQDP_ENABLE_BACKEND=CUDA \
        -DQDP_BUILD_EXAMPLES=OFF \
-       -DQDP_ENABLE_LLVM13=ON
+       -DQDP_ENABLE_LLVM13=ON \
+       -DQDP_PROP_OPT=$PKG_PROP_OPT
 
 
 make -j 32
