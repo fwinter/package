bc8dedc0bdea48ac6fa0d87fdaa6b09ef1b01c49
Frank Winter <fwinter@jlab.org>
2022-07-26 14:26:43 -0400

    LLVM 14.0.6

--- download_sources.sh
+++ download_sources.sh
@@ -6,8 +6,12 @@ pushd src
 git clone https://github.com/usqcd-software/qmp.git
 git clone --recursive --branch devel https://github.com/JeffersonLab/qdp-jit.git
 git clone --recursive --branch devel https://github.com/JeffersonLab/chroma.git
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.1/llvm-13.0.1.src.tar.xz
+#wget https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.1/llvm-13.0.1.src.tar.xz
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.6/llvm-14.0.6.src.tar.xz
+
 echo "Unpacking LLVM. This takes a moment."
-tar xf llvm-13.0.1.src.tar.xz
-rm -f llvm-13.0.1.src.tar.xz
+#tar xf llvm-13.0.1.src.tar.xz
+#rm -f llvm-13.0.1.src.tar.xz
+tar xf llvm-14.0.6.src.tar.xz
+rm -f llvm-14.0.6.src.tar.xz
 popd
