6310bd7168ea3eadcbe192336a59849759e6a246
Frank Winter <fwinter@jlab.org>
2022-09-06 16:54:53 -0400

    change env files llvm 13 -> 14

--- cuda-jit-noquda/env_perlmutter.sh
+++ cuda-jit-noquda/env_perlmutter.sh
@@ -15,4 +15,4 @@ export TOPDIR_HIP=`pwd`
 export SRCROOT=${TOPDIR_HIP}/../src
 export BUILDROOT=${TOPDIR_HIP}/build
 export INSTALLROOT=${TOPDIR_HIP}/install
-export LD_LIBRARY_PATH=${INSTALLROOT}/llvm-13/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${INSTALLROOT}/llvm-14/lib:${LD_LIBRARY_PATH}
