2f51179e05c0af64ddf097d8f9657cb64f89725f
Frank Winter <fwinter@jlab.org>
2024-05-03 14:40:27 -0400

    ROCm: dynamic linker library path adjust

--- download_sources.sh
+++ download_sources.sh
@@ -9,15 +9,12 @@ git clone --recursive --branch devel https://github.com/JeffersonLab/chroma.git
 #wget https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.1/llvm-13.0.1.src.tar.xz
 #wget https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.6/llvm-project-14.0.6.src.tar.xz
 #wget https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/llvm-project-15.0.6.src.tar.xz
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.6/llvm-project-16.0.6.src.tar.xz
+#wget https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.4/llvm-project-16.0.4.src.tar.xz
+#wget https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/llvm-project-17.0.6.src.tar.xz
 
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/llvm-project-17.0.6.src.tar.xz
 echo "Unpacking LLVM. This takes a moment."
-#tar xf llvm-13.0.1.src.tar.xz
-#rm -f llvm-13.0.1.src.tar.xz
-#tar xf llvm-project-14.0.6.src.tar.xz
-#rm -f llvm-project-14.0.6.src.tar.xz
-#tar xf llvm-project-15.0.6.src.tar.xz
-#rm -f llvm-project-15.0.6.src.tar.xz
-tar xf llvm-project-16.0.6.src.tar.xz
-rm -f llvm-project-16.0.6.src.tar.xz
+tar xf llvm-project-17.0.6.src.tar.xz
+rm -f llvm-project-17.0.6.src.tar.xz
+
 popd
--- rocm-jit-noquda/env_frontier.sh
+++ rocm-jit-noquda/env_frontier.sh
@@ -30,4 +30,6 @@ export TOPDIR_HIP=`pwd`
 export SRCROOT=${TOPDIR_HIP}/../src
 export BUILDROOT=${TOPDIR_HIP}/build
 export INSTALLROOT=${TOPDIR_HIP}/install
-export LD_LIBRARY_PATH=${ROCM_PATH}/llvm/lib:${LD_LIBRARY_PATH}
+#export LD_LIBRARY_PATH=${ROCM_PATH}/llvm/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${INSTALLROOT}/llvm-17/lib:${LD_LIBRARY_PATH}
+
